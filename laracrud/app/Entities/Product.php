eb012d21c07d1b403c996fcd6fc7c9aaaaad2371
issabbir <sabbirahmed003@gmail.com>
2023-05-30 14:04:57 +0600

    Project Upload.
    Project Upload.

---
+++ laracrud/app/Entities/Product.php
@@ -0,0 +1,17 @@
+<?php
+ 
+
+namespace App\Entities;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table = "products";
+    protected $primaryKey = "id";
+    public $timestamps = false;
+    protected $fillable = [
+        'name', 'detail'
+    ];
+}
